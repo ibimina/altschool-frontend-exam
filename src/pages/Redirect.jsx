ee007dd256623c9418ca7c3883c4b489bafc6ce0
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-11-02 18:05:03 +0100

    No matching Route redirect component

---
+++ src/pages/Redirect.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Redirect() {
+  return (
+    <div>
+        <Link to="/">home</Link>
+    </div>
+  )
+}
