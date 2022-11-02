1c70d6b0e2568b8ae686b828df75797b3abf95ee
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-11-02 18:08:03 +0100

    Changed Single to Repository details

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import RepositoryList from "../pages/RepositoryList";
 import Redirect from "../pages/Redirect";
-import Single from "../pages/Single";
 import TestError from "../pages/TestError";
+import RepositoryDetails from "../pages/RepositoryDetails";
 
 export default function AppRoute() {
   return (
     <>
       <Routes>
         <Route path="/"  element={<RepositoryList />}>
-          <Route path="ibimina/:id" element={<Single />} />
+          <Route path="ibimina/:id" element={<RepositoryDetails />} />
         </Route>
         <Route path="test" element={<TestError />} />
         <Route path="*" element={<Redirect />} />
