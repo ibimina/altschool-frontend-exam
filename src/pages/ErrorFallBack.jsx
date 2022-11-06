0c065bff4260bd1c605aa7d6c44de97f28cdc1fe
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-11-06 20:12:30 +0100

    docs: added a error image

--- src/pages/ErrorFallBack.jsx
+++ src/pages/ErrorFallBack.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function ErrorFallBack({error,resetErrorBoundary}) {
   return (
-    <div role="alert" className='fallback'>
-      <p>something went wrong</p>
+    <div role="alert" className="fallback">
+      <img
+        src="/assets/icons8-error-cloud-60.png"
+        alt="loading icon"
+        className="load_img"
+      />
+      <p>Something went wrong</p>
       <pre>{error.message}</pre>
       <button onClick={resetErrorBoundary}>try again</button>
     </div>
