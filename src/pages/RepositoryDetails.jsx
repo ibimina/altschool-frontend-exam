be4f0e9676f1fb1c598138af5228f786667e329b
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-11-06 20:17:19 +0100

    docs: formated date data using date-fns package

--- src/pages/RepositoryDetails.jsx
+++ src/pages/RepositoryDetails.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { HelmetProvider,Helmet } from "react-helmet-async";
+import React from "react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 import { useLocation } from "react-router-dom";
-
+import { formatDistanceStrict } from "date-fns";
 export default function RepositoryDetails() {
   const location = useLocation();
   let { doc } = location.state;
- 
-  let lastUpdate = new Date(doc.updated_at).toDateString()
+  let Update = new Date(doc.updated_at);
+  let now = new Date();
+  let lastUpdate = formatDistanceStrict(now, Update);
 
   return (
     <HelmetProvider>
@@ -15,115 +16,115 @@ export default function RepositoryDetails() {
         <meta name="description" content="a single repo details" />
       </Helmet>
       <div className="repo_de">
-        <div className="flex space-btw">
-          <div className="flex no-space">
-            <div>
-              <p>{doc.owner.login}</p>
-              <a href={doc.svn_url} target="_blank">
-                {doc.name}
-              </a>
+        <div className="repo_bg">
+          <div className="flex space-btw">
+            <div className="flex no-space">
+              <div>
+                <p>{doc.owner.login}</p>
+                <a href={doc.svn_url} target="_blank">
+                  {doc.name}
+                </a>
+              </div>
+
+              <p className="public">{doc.visibility}</p>
             </div>
 
-            <p className="public">{doc.visibility}</p>
+            {doc.stargazers_count > 0 && (
+              <div className="flex gap  no-space">
+                <img
+                  src="/assets/icons8-golden-star.png"
+                  className="star"
+                  alt="golden star icon"
+                />
+                <p>Starred</p>
+              </div>
+            )}
+            {doc.stargazers_count === 0 && (
+              <div className="flex gap">
+                <img
+                  src="/assets/icons8-bg-star.png"
+                  className="star"
+                  alt="star icon"
+                />
+                <p>Star</p>
+              </div>
+            )}
           </div>
-
-          {doc.stargazers_count > 0 && (
-            <div className="flex gap  no-space">
-              <img
-                src="/assets/icons8-golden-star.png"
-                className="star"
-                alt="golden star icon"
-              />
-              <p>Starred</p>
+          {doc.description && <p className="bio">{doc.description}</p>}
+          <div className="flex">
+            <div className="flex gap">
+              <img src="/assets/icons8-bg-star.png" className="star" />{" "}
+              <span> {doc.stargazers_count} star</span>
             </div>
-          )}
-          {doc.stargazers_count === 0 && (
+
             <div className="flex gap">
               <img
-                src="/assets/icons8-bg-star.png"
+                src="/assets/icons8-forked.png"
                 className="star"
-                alt="star icon"
+                alt="fork icon"
               />
-              <p>Star</p>
+              <p>{doc.forks} fork</p>
             </div>
+            <p>{doc.open_issues} issues</p>
+            <p>Updated {lastUpdate} ago</p>
+          </div>
+
+          {doc.language === "CSS" && (
+            <>
+              <p className="flex">
+                {" "}
+                <span
+                  style={{
+                    backgroundColor: "purple",
+                    width: "15px",
+                    borderRadius: "50%",
+                    height: "15px",
+                    display: "inline-block",
+                  }}
+                ></span>
+                {doc.language}
+              </p>
+            </>
           )}
+          {doc.language === "Javascript" && (
+            <>
+              <p className="flex">
+                {" "}
+                <span
+                  style={{
+                    backgroundColor: "yellow",
+                    width: "15px",
+                    borderRadius: "50%",
+                    height: "15px",
+                    display: "inline-block",
+                  }}
+                ></span>
+                {doc.language}
+              </p>
+            </>
+          )}
+          {doc.language === "HTML" && (
+            <>
+              <p className="flex">
+                {" "}
+                <span
+                  style={{
+                    backgroundColor: "red",
+                    width: "15px",
+                    borderRadius: "50%",
+                    height: "15px",
+                    display: "inline-block",
+                  }}
+                ></span>
+                {doc.language}
+              </p>
+            </>
+          )}
+
+          <a href={doc.html_url} rel="noreferrer" target="_blank">
+            view repository on githbub
+          </a>
         </div>
-        <div className="flex">
-          <div className="flex gap">
-            <img src="/assets/icons8-bg-star.png" className="star" />{" "}
-            <span> {doc.stargazers_count} star</span>
-          </div>
-
-          <div className="flex gap">
-            <img
-              src="/assets/icons8-forked.png"
-              className="star"
-              alt="fork icon"
-            />
-            <p>{doc.forks} fork</p>
-          </div>
-          <p>{doc.open_issues} issues</p>
-           <p>
-            Updated on {lastUpdate}
-          </p> 
-        </div>
-
-        {doc.description && <p>{doc.description}</p>}
-        {doc.language === "CSS" && (
-          <>
-            <p className="flex">
-              {" "}
-              <span
-                style={{
-                  backgroundColor: "purple",
-                  width: "15px",
-                  borderRadius: "50%",
-                  height: "15px",
-                  display: "inline-block",
-                }}
-              ></span>
-              {doc.language}
-            </p>
-          </>
-        )}
-        {doc.language === "Javascript" && (
-          <>
-            <p className="flex">
-              {" "}
-              <span
-                style={{
-                  backgroundColor: "yellow",
-                  width: "15px",
-                  borderRadius: "50%",
-                  height: "15px",
-                  display: "inline-block",
-                }}
-              ></span>
-              {doc.language}
-            </p>
-          </>
-        )}
-        {doc.language === "HTML" && (
-          <>
-            <p className="flex">
-              {" "}
-              <span
-                style={{
-                  backgroundColor: "red",
-                  width: "15px",
-                  borderRadius: "50%",
-                  height: "15px",
-                  display: "inline-block",
-                }}
-              ></span>
-              {doc.language}
-            </p>
-          </>
-        )}
-
-        <a href={doc.html_url} rel="noreferrer" target="_blank">
-          view repository on githbub
-        </a>
       </div>
     </HelmetProvider>
   );
