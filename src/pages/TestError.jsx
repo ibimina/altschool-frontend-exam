d19306991d7b3f605c0450de1ed913b665b01936
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-11-06 23:26:02 +0100

    docs: changed error message

--- src/pages/TestError.jsx
+++ src/pages/TestError.jsx
@@ -6,11 +6,11 @@ import ErrorFallBack from "./ErrorFallBack";
 
 export default function TestError() {
   const [user, setUser] = useState("");
-  const Bom = ({ user }) => {
+  const NameBug = ({ user }) => {
     if (user === "ibimina") {
-       throw new Error("Why na?");
+       throw new Error("User triggered an error ");
     } else {
-      return    <p className="hello">Hello {user} </p>    
+      return    <p className="hello">Hello {user}</p>    
          };
         }
 
@@ -19,26 +19,31 @@ export default function TestError() {
       <HelmetProvider>
         <Helmet>
           <title>Error boundary test</title>
-          <meta name="description" content="A page that test react-error-boundary" />
+          <meta
+            name="description"
+            content="A page that test react-error-boundary"
+          />
         </Helmet>
         <NavBar />
         <div className="test">
+
           <h2 className="title">Let's Test your Error Boundary 🤔</h2>
+         <p className="bio">Do not enter "ibimina" as this will trigger an error</p>
           <input
             type="text"
             value={user}
             onChange={(e) => setUser(e.target.value)}
-            placeholder="Don't enter ibimina"
+            placeholder="Enter your first name"
           />
           <ErrorBoundary
             FallbackComponent={ErrorFallBack}
             onReset={() => setUser("")}
             resetKeys={[user]}
           >
-            <Bom user={user} />
+            <NameBug user={user} />
           </ErrorBoundary>
-          <Footer />
         </div>
+        <Footer />
       </HelmetProvider>
     </>
   );
