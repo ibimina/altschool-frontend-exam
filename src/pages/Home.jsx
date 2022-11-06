0168e5bf815f73e71993e0ed991bcdce6b7629da
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-11-06 20:18:44 +0100

    docs: imported receivedevent component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { NavBar, Profile, Footer } from "../components";
+import { NavBar, Profile, Footer, ReceivedEvents } from "../components";
+import useFetch from "../hooks/useFetch";
 
 export default function Home() {
+  const url = "https://api.github.com/users/ibimina";
+    const { state } = useFetch(url)
+    const { loading, docs } = state;
+
   return (
     <>
       <HelmetProvider>
@@ -10,7 +15,10 @@ export default function Home() {
           <meta name="description" content="Github profile" />
         </Helmet>
         <NavBar />
-        <Profile />
+        <div className="home_content">
+          <Profile docs={docs} loading={loading} />
+          <ReceivedEvents  />
+        </div>
         <Footer />
       </HelmetProvider>
     </>
