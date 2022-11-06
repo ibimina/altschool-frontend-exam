1a52dddf0e3fdfaf2e2821e5b615e120ea3605a8
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-11-06 23:56:02 +0100

    docs: export default loading component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,4 @@ export {default as NavBar} from "./NavBar.jsx"
 export { default as Footer } from "./Footer.jsx";
 export { default as Profile } from "./Profile.jsx";
 export { default as ReceivedEvents } from "./ReceivedEvents.jsx";
+export { default as Loading } from "./Loading.jsx";
