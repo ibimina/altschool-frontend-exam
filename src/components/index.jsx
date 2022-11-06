cd1d408ae5338aaf02573af65163db68204bf24a
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-11-06 20:14:28 +0100

    docs; export default receivedevents components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
 export {default as NavBar} from "./NavBar.jsx"
 export { default as Footer } from "./Footer.jsx";
 export { default as Profile } from "./Profile.jsx";
+export { default as ReceivedEvents } from "./ReceivedEvents.jsx";
