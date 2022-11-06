2216e09ad9b1476310be0f1ddf7072cdf254b2a2
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-11-06 23:55:07 +0100

    docs: craeted a loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+   <div className="loading">
+          <img
+            src="/assets/icons8-preloader-64.png"
+            alt="loading icon"
+            className="load_img"
+          />
+        </div>
+  )
+}
+
