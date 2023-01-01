92d97338ab78fdc2dae50ace5353fae0c6d8b663
ibimina <96787210+ibimina@users.noreply.github.com>
2023-01-01 13:21:55 +0100

    docs: refactor code

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,30 +1,74 @@
-import React from 'react'
+import React from "react";
 import useFetch from "../hooks/useFetch";
-import { formatDistanceStrict,getDate,getMonth} from "date-fns";
+import {  getDate, getMonth } from "date-fns";
+import Language from "./Language";
 
-export default function Event({url,doc}) {
-      const { state } = useFetch(url);
-      const { loading, docs } = state;
-      console.log(loading,docs)
-      const months=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+export default function Event({ url, doc }) {
+  const { state } = useFetch(url);
+  const { loading, docs } = state;
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
   return (
     <div className="card">
       <div className="flexx">
-        <a href={doc.html_url} target="_blank" rel="noopener noreferrer">
+        <a href={docs.html_url} target="_blank" rel="noopener noreferrer">
           {doc.repo.name}
         </a>
-        {docs.stargazers_count > 0 && <div>Star</div>}
+        {docs.stargazers_count > 0 && (
+          <div className="flex star-bor">
+            <img src="/assets/star.png" alt="" className="star" />{" "}
+            Starred
+          </div>
+        )}
         {docs.stargazers_count == 0 && (
           <div className="flex star-bor">
-            <img src="/assets/icons8-bg-star.png" alt="" /> Star
+            <img src="/assets/icons8-bg-star.png" alt="" className="star" />{" "}
+            Star
           </div>
         )}
       </div>
-      {docs.description !== null && <p>{docs.description}</p>}
-      <p>
-        Updated {getDate(new Date(docs?.updated_at))}{" "}
-        {months[getMonth(new Date(docs?.updated_at))]}
-      </p>
+      {docs.description !== null && <p className="event_desc">{docs.description}</p>}
+      <div className="flex">
+        {doc.type !== "CreateEvent" && (
+          <>
+            <Language language={docs.language} />
+            <div className="f">
+              <img src="/assets/icons8-bg-star.png" alt="" className="star" />
+              <p className="small">
+                {docs.stargazers_count >= 1000
+                  ? (docs.stargazers_count / 1000).toFixed(1) + "k"
+                  : docs.stargazers_count > 10000
+                  ? (docs.stargazers_count / 10000).toFixed(1) + "k"
+                  : docs.stargazers_count > 100000
+                  ? (docs.stargazers_count / 100000).toFixed(1) + "k"
+                  : docs.stargazers_count}
+              </p>
+            </div>
+          </>
+        )}
+        <p className="small">
+          Updated {getDate(new Date(docs?.updated_at))}{" "}
+          {months[getMonth(new Date(docs?.updated_at))]}
+        </p>
+      </div>
+      {doc.type === "ForkEvent" && (
+        <>
+          <p>{docs.open_issues_count} issues need help</p>
+        </>
+      )}
     </div>
   );
 }
