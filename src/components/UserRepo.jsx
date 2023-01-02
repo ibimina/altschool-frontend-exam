bb9ac6a14f0250f446e3ae152ab8b0c5c36dc9bb
ibimina <96787210+ibimina@users.noreply.github.com>
2023-01-02 02:36:59 +0100

    docs: searched user github repo

---
+++ src/components/UserRepo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import useFetch from '../hooks/useFetch'
+
+export default function UserRepo({url}) {
+let rep = `https://api.github.com/users/${url}/repos`;
+    const{state}=useFetch(rep)
+const {loading,docs}=state
+
+  return (
+    <div className='link_con'>
+        {docs && docs.map((repo)=>
+        <a href={repo.html_url}target="_blank" rel="noopener noreferrer" key={repo.id} className="userlink">{repo.name} </a>
+        )}
+    </div>
+  )
+}
