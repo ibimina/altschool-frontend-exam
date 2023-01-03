27cba36fc9afae73701d00cc789f622f9a2fcc6c
ibimina <96787210+ibimina@users.noreply.github.com>
2023-01-03 11:20:44 +0100

    docs: added language and userrepo

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,3 +3,5 @@ export { default as Footer } from "./Footer.jsx";
 export { default as Profile } from "./Profile.jsx";
 export { default as ReceivedEvents } from "./ReceivedEvents.jsx";
 export { default as Loading } from "./Loading.jsx";
+export { default as UserRepo } from "./UserRepo.jsx";
+export { default as Language } from "./Language.jsx";
