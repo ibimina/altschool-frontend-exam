bec5bb88261ebb61cbf316cd1c38b73ad402134d
ibimina <96787210+ibimina@users.noreply.github.com>
2023-01-03 11:02:22 +0100

    docs: refactored code

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,76 +1,23 @@
 import React from 'react'
-
+import"./language.css"
 export default function Language({language}) {
   return (
     <>
-      {language === "CSS" && (
-        <>
-          <p className="f small">
-            {" "}
-            <span
-              style={{
-                backgroundColor: "purple",
-                width: "15px",
-                borderRadius: "50%",
-                height: "15px",
-                display: "inline-block",
-              }}
-            ></span>
-            {language}
-          </p>
-        </>
-      )}
-      {language === "JavaScript" && (
-        <>
-          <p className="f small">
-            {" "}
-            <span
-              style={{
-                backgroundColor: "yellow",
-                width: "15px",
-                borderRadius: "50%",
-                height: "15px",
-                display: "inline-block",
-              }}
-            ></span>
-            {language}
-          </p>
-        </>
-      )}
-      {language === "HTML" && (
-        <>
-          <p className="f small">
-            {" "}
-            <span
-              style={{
-                backgroundColor: "red",
-                width: "15px",
-                borderRadius: "50%",
-                height: "15px",
-                display: "inline-block",
-              }}
-            ></span>
-            {language}
-          </p>
-        </>
-      )}
-      {language === "TypeScript" && (
-        <>
-          <p className="f small">
-            {" "}
-            <span
-              style={{
-                backgroundColor: "blue",
-                width: "15px",
-                borderRadius: "50%",
-                height: "15px",
-                display: "inline-block",
-              }}
-            ></span>
-            {language}
-          </p>
-        </>
-      )}
+      <p className="f small">
+        {" "}
+        <span
+          className={`language_color ${language === "CSS"
+              ? "css"
+              : language === "JavaScript"
+              ? "javascript"
+              : language === "HTML"
+              ? "html"
+              : language === "TypeScript"
+              ?"typescript":
+              ""}`}
+        ></span>
+        {language}
+      </p>
     </>
   );
 }
